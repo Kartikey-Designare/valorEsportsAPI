5a3ddeb498d638238fc6cda9fa5e4d6a62d65c76
Kartikey <kartikey.soni@designare.in>
2024-05-29 17:29:48 +0530

    added live score of current game feature for live games

--- index.js
+++ index.js
@@ -3,9 +3,70 @@ const request = require("request");
 const cheerio = require("cheerio");
 const app = express();
 
-app.get("/match", function (req, res) {
+// LIVE NOW
+app.get("/live", function (req, res) {
   let url = "https://www.vlr.gg";
-  let query = req.query.q;
+
+  request(url, function (error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
+
+      var liveMatches = [];
+
+      $("div.js-home-matches-upcoming > div > a").each((index, element) => {
+        let match = cheerio.load(element);
+
+        let time_left;
+
+        let teams = [];
+        let matchScores = [];
+        let gameScores = [];
+
+        match("div.h-match-team").each((index, element) => {
+          let team = cheerio.load(element);
+
+          if (index == 0) {
+            time_left = `${team("div.h-match-eta").text().trim()} from now`;
+          }
+
+          teams.push(team("div.h-match-team-name").text().trim());
+          matchScores.push(team("div.h-match-team-score").text().trim());
+          gameScores.push(team("div.h-match-team-rounds > span").text().trim());
+        });
+
+        liveMatches.push({
+          match_event: match("div.h-match-preview > div.h-match-preview-event")
+            .text()
+            .trim(),
+          team1: teams[0],
+          team2: teams[1],
+          score1: matchScores[0],
+          score2: matchScores[1],
+          gameScore1: gameScores[0],
+          gameScore2: gameScores[1],
+          time_until_match: time_left,
+        });
+      });
+
+      liveMatches = liveMatches.filter((curr, index) => {
+        return curr.time_until_match == "LIVE from now";
+      });
+
+      var json = {
+        data: {
+          status: 200,
+          segments: liveMatches,
+        },
+      };
+    }
+
+    res.send(json);
+  });
+});
+
+// UPCOMING MATCHES
+app.get("/upcoming", function (req, res) {
+  let url = "https://www.vlr.gg";
 
   request(url, function (error, response, html) {
     if (!error) {
@@ -40,15 +101,9 @@ app.get("/match", function (req, res) {
         });
       });
 
-      if (query == "upcoming") {
-        upcomingMatches = upcomingMatches.filter((curr, index) => {
-          return curr.time_until_match !== "LIVE from now";
-        });
-      } else if (query == "live_score") {
-        upcomingMatches = upcomingMatches.filter((curr, index) => {
-          return curr.time_until_match == "LIVE from now";
-        });
-      }
+      upcomingMatches = upcomingMatches.filter((curr, index) => {
+        return curr.time_until_match !== "LIVE from now";
+      });
 
       var json = {
         data: {
@@ -62,6 +117,7 @@ app.get("/match", function (req, res) {
   });
 });
 
+// MATCH RESULTS
 app.get("/result", function (req, res) {
   let url = "https://www.vlr.gg/matches/results";
 
